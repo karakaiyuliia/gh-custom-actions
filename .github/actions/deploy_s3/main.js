a3f47232a3a7aab9b6eeac5a70f9a4dd12e59a5c
Yuliia Karakai <karakaiyuliia@gmail.com>
2023-07-10 19:47:35 -0400

    added js action

---
+++ .github/actions/deploy_s3/main.js
@@ -0,0 +1,8 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
+function run() {
+  console.log("hello from a js action")
+}
+
+run();
