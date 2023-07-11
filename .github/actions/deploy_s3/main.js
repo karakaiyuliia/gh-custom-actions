60ced440cf2463c639c07e611fdcce9945b2743f
Yuliia Karakai <karakaiyuliia@gmail.com>
2023-07-10 20:22:51 -0400

    log url

--- .github/actions/deploy_s3/main.js
+++ .github/actions/deploy_s3/main.js
@@ -3,13 +3,17 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 function run() {
   // get input values
-  const bucket = core.getInput('bucket', { required: true })
-  const bucketRegion = core.getInput('bucket-region', { required: true })
-  const distFolder = core.getInput('dist-folder', { required: true })
+  const bucket = core.getInput('bucket', { required: true });
+  const bucketRegion = core.getInput('bucket-region', { required: true });
+  const distFolder = core.getInput('dist-folder', { required: true });
 
   // upload files
-  const s3Uri = `s3://${bucket}`
-  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+  // website url
+  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+  core.setOutput('website-url', websiteUrl);
 }
 
 run();
