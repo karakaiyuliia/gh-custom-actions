6a67cdd1781b2186842ac99321b9398d6e669736
Yuliia Karakai <karakaiyuliia@gmail.com>
2023-07-10 20:16:57 -0400

    added custom deploy action

--- .github/actions/deploy_s3/main.js
+++ .github/actions/deploy_s3/main.js
@@ -2,7 +2,14 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
 function run() {
-  console.log("hello from a js action")
+  // get input values
+  const bucket = core.getInput('bucket', { required: true })
+  const bucketRegion = core.getInput('bucket-region', { required: true })
+  const distFolder = core.getInput('dist-folder', { required: true })
+
+  // upload files
+  const s3Uri = `s3://${bucket}`
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
 }
 
 run();
